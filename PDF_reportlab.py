fdf72490b5d81394f102aa3e32060639015c09a6
Yu-Wei Lai <m10803126@mail.ntust.edu.tw>
2023-08-29 11:38:13 +0800

    PDF_add_word

--- PDF_reportlab.py
+++ PDF_reportlab.py
@@ -1,13 +1,16 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics  # 新增字體
+from reportlab.pdfbase.ttfonts import TTFont
 
 # 創建一個PDF文件
 c = canvas.Canvas('Test.pdf', pagesize=letter)
 
+pdfmetrics.registerFont(TTFont('kaiu', 'kaiu.ttf'))
 # 增加文本
-A='敘述文字'
-c.drawString(100, 750, A)
-#A.encode('unicode_escape')
+c.setFont('kaiu', 14)
+c.drawString(100, 750, '下課')
+# A.encode('unicode_escape')
 
 # 增加圖形
 c.setStrokeColorRGB(1, 0, 0)  # 設定邊框顏色
