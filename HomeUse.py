8ffb92433878fd929231843b89a16ee922451238
Yu-Wei Lai <m10803126@mail.ntust.edu.tw>
2023-09-04 15:09:10 +0800

    add_Home_replay

---
+++ HomeUse.py
@@ -0,0 +1,41 @@
+# class Flyable:
+#     def fly(self):
+#         print("我會飛行!")
+# class Swimmable:
+#     def swim(self):
+#         print("我會游泳!")
+#
+# class HybridCreature(Flyable, Swimmable):
+#     def __init__(self, name):
+#         self.name = name
+#     def show_name(self):
+#         print("我的名字", self.name)
+# hybrid = HybridCreature("混合生物")
+# hybrid.show_name()
+# hybrid.swim()
+# hybrid.fly()
+# # _______________________________________________________________________________________
+# class fiying:
+#     def fly(self):
+#         print('我會飛')
+#
+#
+# class swimming:
+#     def swim(self):
+#         print('我會游泳')
+#
+#
+# class mixanimal(fiying, swimming):
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def Name(self):
+#         print('我的名字是', self.name)
+#
+#
+# Mix = mixanimal('混和生物')
+# Mix.Name()
+# Mix.swim()
+# Mix.fly()
+# _______________________________________________________________________________________
+
